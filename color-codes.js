fb7278ee2b048b34c3f1fb3e57c1106393d729c1
bilboblockins <abretthiebert@gmail.com>
2020-08-06 16:06:58 -0400

    add files

---
+++ color-codes.js
@@ -0,0 +1,26 @@
+const Reset = "\x1b[0m"
+const Bright = "\x1b[1m"
+const Dim = "\x1b[2m"
+const Underscore = "\x1b[4m"
+const Reverse = "\x1b[7m"
+
+const FgBlack = "\x1b[30m"
+const FgRed = "\x1b[31m"
+const FgGreen = "\x1b[32m"
+const FgYellow = "\x1b[33m"
+const FgBlue = "\x1b[34m"
+const FgMagenta = "\x1b[35m"
+const FgCyan = "\x1b[36m"
+const FgWhite = "\x1b[37m"
+
+const BgBlack = "\x1b[40m"
+const BgRed = "\x1b[41m"
+const BgGreen = "\x1b[42m"
+const BgYellow = "\x1b[43m"
+const BgBlue = "\x1b[44m"
+const BgMagenta = "\x1b[45m"
+const BgCyan = "\x1b[46m"
+const BgWhite = "\x1b[47m"
+
+console.log(FgCyan, Dim, "Some cyan text here", Reset);
+console.log(FgRed, Underscore, "Some underlined red text here", Reset);
